a1796aa41f047f5ced38f04ef48eaa4fe2c09953
Hopelynn-dev <chibondwehope@gmail.com>
2026-05-13 19:20:10 +0200

    Initial commit

---
+++ intro.js
@@ -0,0 +1,7 @@
+const Name = "Hope Chibondwe";
+const Age = 20;
+const favLanguage = "JavaScript";
+
+console.log("Name: " + Name);
+console.log("Age: " + Age);
+console.log("Favourite programming language: " + favLanguage);
